e203566686d612b38221949797dccac49c12b55c
Dylan <weinac_d@etna-alternance.net>
2023-06-27 09:53:02 +0200

    first commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    private $em;
+    public function __construct(EntityManagerInterface $em) 
+    {
+        $this->em = $em;
+    }
+
+
+    #[Route('/movies', name: 'app_movies')]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $repository = $em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+
+        dd($movies);
+
+        return $this -> render('index.html.twig');
+    }
+
+}
---
+++ src/DataFixtures/MovieFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class MovieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $movie = new Movie();
+        $movie->setTitle('The Dark Knight');
+        $movie->setReleaseYear(2008);
+        $movie->setDescription('This is the description of the Dark Knight');
+        $movie->setImagePath('https://fr.web.img2.acsta.net/medias/nmedia/18/63/97/89/18949761.jpg');
+
+        // Add data to Pivot Table ( = table de liaison entre les actor et movie )
+        $movie->addActor($this->getReference('actor_1'));
+        $movie->addActor($this->getReference('actor_2'));
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2->setTitle('Avengers : Endgame');
+        $movie2->setReleaseYear(2019);
+        $movie2->setDescription('This is the description of Avengers');
+        $movie2->setImagePath('https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0%2C0%2C540%2C810');
+
+        // Add data to Pivot Table ( = table de liaison entre les actor et movie )
+        $movie2->addActor($this->getReference('actor_3'));
+        $movie2->addActor($this->getReference('actor_4'));
+        $manager->persist($movie2);
+
+        $manager->flush();
+    }
+}
